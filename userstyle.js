a5e749a75f4f3614a9668a30aa24edb92e4746d1
_iPhoenix_ <theiphoenixconspiracy@gmail.com>
2020-09-16 12:40:26 -0400

    Implement Tweaks and provide example

--- userstyle.js
+++ userstyle.js
@@ -1,2 +1,24 @@
-// make sure this is always the line one in the file for future maintainers.
-console.log("Cemetech Userstyles v2.2.0 by _iPhoenix_ and contributors loaded.")
+new Tweak("Fix unicode in post titles", /cemetech\.net\/forum\/viewtopic\.php/, () => {
+    /* This function was yanked from SAX but it was also originally written by me (_iPhoenix_),
+     *   though I'm sure I got the idea from some other piece of code I'd seen.
+     *
+     * Unescapes HTML entities in the unsafe input
+     * `text`, creating an even more unsafe output.
+     *
+     * This means that unicode characters recieved by the client as already
+     * escaped (ex. in post titles) will be properly rendered.
+     *
+     * Note that using a textarea is super duper important- using any other
+     * element opens up trivial XSS attacks, and it allows us to extract the
+     * value, which preserves any braces but unescapes entities.
+     */
+    function unescapeEntities(html) {
+        var txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+    }
+
+    const maintitle = document.querySelector(".mainheadmiddle.roundedtop .maintitle");
+
+    maintitle.innerText = unescapeEntities(maintitle.innerText);
+});
