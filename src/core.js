977a617299ebdb54d33b3d748cad98173896adb3
_iPhoenix_ <theiphoenixconspiracy@gmail.com>
2021-05-05 12:12:32 -0400

    Bump version

--- src/core.js
+++ src/core.js
@@ -73,4 +73,4 @@ window.addEventListener("load", () => {
 });
 
 // make sure this is always the line one in the file for future maintainers.
-console.log("Cemetech Userstyles v3.1.0 by _iPhoenix_ and contributors loaded.")
+console.log("Cemetech Userstyles v3.1.1 by _iPhoenix_ and contributors loaded.")
