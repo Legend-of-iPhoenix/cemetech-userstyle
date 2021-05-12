7f7303832ef9800481fee1dcba5da55e70093f9e
_iPhoenix_ <theiphoenixconspiracy@gmail.com>
2021-05-12 10:52:25 -0400

    Add sc3 fullscreen
    Co-authored-by: NoahCK <noahkiser95mygc@gmail.com>

--- src/userstyle.js
+++ src/userstyle.js
@@ -96,6 +96,20 @@ new Tweak("Restyle UTI pages", /cemetech\.net\/projects\/uti/, () => {
 	document.body.append(style);
 });
 
+// styles from noahk's chrome extension
+new Tweak("Add fullscreen button to sc3.", /cemetech\.net\/sc/, () => {
+	const button = document.createElement("a"); // great web design right here
+	const innerDiv = document.createElement("div");
+
+	innerDiv.innerHTML = "<span style='color: white;'>&#x26F6;</span> Toggle Fullscreen"; // U+26F6 "SQUARE FOUR CORNERS" ⛶"
+	innerDiv.classList = "sc3_interface_button";
+	button.appendChild(innerDiv);
+
+	document.getElementsByClassName("project_pane")[0].appendChild(button);
+
+	button.onclick = () => document.body.classList.toggle("sc3Fullscreen");
+});
+
 new Tweak("Completely redo post editor", /cemetech\.net\/forum\/posting\.php/, () => {
 	const form = document.querySelector("form[name=post]");
 
